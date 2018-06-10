bf8fa73ce963cbc78fe38aa1395769a6a5b09bf1
Ashok <ashok.b@zohocorp.com>
2018-06-10 23:01:41 +0530

    added audio manager
    meta files
    
    removal of meta files

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    public int dialogueCount = 1;
+
+    //this function is called before Start()
+	void Awake () {
+
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        /*foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }*/
+		
+	}
+	
+    public void Play(AudioSource dialogueSource)
+    {
+        dialogueSource.Play();
+    }
+}
---
+++ Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class DialogueScript : MonoBehaviour 
+{
+    AudioSource dialogueSource;
+    Sound[] dialogueSounds;
+
+    private void Start()
+    {
+        dialogueSounds = FindObjectOfType<AudioManager>().sounds;
+    }
+
+    public void PlayDialogue()
+    {
+        Debug.Log("audioManager - s:" + dialogueSounds.Length);
+
+        dialogueSource = gameObject.AddComponent<AudioSource>();
+        string gameObjectName = gameObject.name;
+        Debug.Log("gameObjectName ::" + gameObjectName);
+
+        setAudioProperties(gameObjectName,dialogueSource);
+
+        if(dialogueSource.isPlaying)
+        {
+            return;
+        }
+
+        bool canPlayDialogue = getCanPlayDialogue(gameObjectName);
+
+        if(canPlayDialogue)
+        {
+            FindObjectOfType<AudioManager>().dialogueCount++;
+            FindObjectOfType<AudioManager>().Play(dialogueSource);
+        }
+
+
+
+    }
+
+    private void setAudioProperties(string gameObjectName, AudioSource dialogueSource)
+    {
+        
+        foreach(Sound s in dialogueSounds)
+        {
+            if(s.name.Equals(gameObjectName))
+            {
+                dialogueSource.clip = s.clip;
+                dialogueSource.volume = s.volume;
+                //dialogueSource.pitch = s.pitch;
+                break;
+            }
+        }
+
+    }
+
+    bool getCanPlayDialogue(string gameObjectName)
+    {
+        int dialogueNumber = int.Parse(gameObjectName);
+        if(FindObjectOfType<AudioManager>().dialogueCount == dialogueNumber)
+        {
+            return true;
+        }
+        return false;
+    }
+}
